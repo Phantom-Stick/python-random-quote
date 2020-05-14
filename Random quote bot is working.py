37883399a3bad2dbcff54032904f442a83aa9061
Phantom-Stick <65308147+Phantom-Stick@users.noreply.github.com>
2020-05-14 11:57:14 -0600

    Add files via upload

---
+++ Random quote bot is working.py
@@ -0,0 +1,14 @@
+import random
+last = 13
+rnd = random.randint(0, last)
+def main():
+   #print("Keep it logically awesome.")
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  print(quotes[rnd])
+
+if __name__== "__main__":
+  main()
